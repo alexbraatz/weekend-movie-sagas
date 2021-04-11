4ced3a459cce5753f4e3dc76a820ca8e6c41c195
alex braatz <alexjbraatz@gmail.com>
2021-04-10 19:15:53 -0500

    data sends through movie info + genre id to db

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -28,28 +28,68 @@ function AddMovie() {
     const [ newMoviePosterUrl, setNewMoviePosterUrl ] = useState( '' );
     const [ newMovieDescription, setNewMovieDescription ] = useState( '' );
     const [ selectedGenres, setSelectedGenres ] = useState( [] );
-    const [ genreId, setGenreId ] = useState( 0 );
 
     const saveMovie = () => {
 
+        let count = 0;
+
         selectedGenres.map( newGenre => {
-            console.log( 'in selectedGenres map', newGenre )
-            if( newGenre.label === 'Adventure'){
-                setGenreId( prevState => prevState + 1 );
+            console.log( 'in selectedGenres map', newGenre.label )
+            switch( newGenre.label ){
+                case 'Adventure':
+                    count += 1
+                    break;
+                case 'Animated':
+                    count += 2
+                    break;
+                case 'Biographical':
+                    count += 3
+                    break;
+                case 'Comedy':
+                    count += 4
+                    break;
+                case 'Disaster':
+                    count += 5
+                    break;
+                case 'Drama':
+                    count += 6
+                    break;
+                case 'Epic':
+                    count += 7
+                    break;
+                case 'Fantasy':
+                    count += 8;
+                    break;
+                case 'Musical':
+                    count += 9;
+                    break;
+                case 'Romantic':
+                    count += 10;
+                    break;
+                case 'Science Fiction':
+                    count += 11;
+                    break;
+                case 'Space-Opera':
+                    count += 12;
+                    break;
+                case 'Superhero':
+                    count += 13;
+                    break;
             }
+            
         })
 
         let newMovie = {
             title: newMovieTitle,
             poster: newMoviePosterUrl,
             description: newMovieDescription,
-            genre_id: genreId
+            genre_id: count
         }
 
         dispatch( { type: 'SAVE_MOVIE', payload: newMovie } );
 
         axios.post( '/api/movie', newMovie ).then( (response )=>{
-            console.log( 'back from addMovie Post', reponse );
+            console.log( 'back from addMovie Post', response );
         }).catch( error => {
             console.log( 'error in our Post', error )
         })
